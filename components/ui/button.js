d814593925a3a5d5bb5e04177edfec127cb204db
A-hmedA-li <76922181+A-hmedA-li@users.noreply.github.com>
2025-04-27 16:24:48 +0300

    Update button ui

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,19 +2,21 @@ import * as React from "react"
 
 
 
-const button = React.forwardRef(({ className, type, ...props }, ref) => {
+const Button = React.forwardRef(({children , className, type, ...props }, ref) => {
   return (
-    <input
+    <button
       type={type}
       className={
         "w-full bg-amber-500 hover:bg-amber-400 text-white flex items-center justify-center gap-2 py-3 rounded-lg" + 
         className
-      )}
+      }
       ref={ref}
       {...props}
-    />
+    >
+{children}
+</button>
   )
 })
-button.displayName = "Button"
+Button.displayName = "Button"
 
 export { button }
